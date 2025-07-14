d0b3e7ece1b1edfaa22b51dc7aaf20a0058febf2
João <joaovictornascimento.rh@gmail.com>
2025-07-13 23:56:20 -0300

    clean

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Landing from "./pages/Landing";
-import ControlePedidos from "./pages/ControlePedidos";
-import Dashboard from "./pages/Dashboard";
-import Mesa from "./pages/Mesa";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/pedido" element={<Home />} />
-      <Route path="/controle-pedidos" element={<ControlePedidos />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/mesa" element={<Mesa />} />
     </Routes>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from "react-router-dom";
-import { CartProvider } from "./components/CartProvider";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CartProvider>
       <HashRouter>
         <App />
       </HashRouter>
-    </CartProvider>
   </React.StrictMode>
 );
 
